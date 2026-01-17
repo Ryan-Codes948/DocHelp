a093710ed9ee5308aa144b3c09eea04d091b2757
Ryan-Codes948 <johanryan1273@gmail.com>
2026-01-17 22:03:34 +0600

    Add admin session check and redirect unauthorized users

--- views/admin.php
+++ views/admin.php
@@ -1,3 +1,7 @@
 <?php
 session_start();
 require "../app/config/database.php";
+if(!isset($_SESSION['user_id']) || ($_SESSION['role'] ?? '') !== 'admin'){
+    header("Location: ../public/login.php");
+    exit;
+}
